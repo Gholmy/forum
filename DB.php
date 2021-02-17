be5165f431890f37d683e51e1edab9fae0ec3276
Gholmy <79195605+Gholmy@users.noreply.github.com>
2021-02-17 16:10:26 +0100

    Add files via upload

---
+++ DB.php
@@ -0,0 +1,21 @@
+<?php
+
+class DB extends PDO {
+    private const HOST = "localhost";
+    private const DATABASE = "forum";
+    private const PORT = 3306;
+    private const USER = "root";
+    private const PASSWORD = "";
+
+    public function __construct() {
+        $dsn = "mysql:host=" . self::HOST . ";dbname=" . self::DATABASE . ";port=" . self::PORT . ";";
+
+        $options = [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+
+        parent::__construct($dsn, self::USER, self::PASSWORD, $options);
+        // new PDO($dsn, self::USER, self::PASSWORD, $options);
+    }
+}
